05495a4ab6d7a1756d7f5faa3f8e7b105e809ee0
Finn Straehle <finn.straehle1@gmx.ch>
2023-08-21 16:37:03 +0200

    created boat model and booking model, plus created both controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :boats
+  has_many :bookings
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
