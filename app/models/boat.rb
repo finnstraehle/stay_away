36213bcb8c9e9a88acfafdcc099591a8a8b7e66f
Rossella Pellizzi <prellizzi@gmail.com>
2023-08-22 15:17:27 +0200

    home and footer working, search form done

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,6 @@ class Boat < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :crew_members, dependent: :destroy
+
+  CATEGORIES = ["Sailboat", "Motorboat", "Catamaran", "Yacht", "Pirate Ship", "Cargo Ship", "Inflatable Unicorn"]
 end
