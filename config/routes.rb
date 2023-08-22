a49821823835d9a0e0d04611a4b6e0c793b2d590
Finn Straehle <finn.straehle1@gmx.ch>
2023-08-22 16:23:34 +0200

    everything fixed – controllers, routes, models, DB

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get "/dashboard", to: "pages#dashboard", as: :dashboard do
-    resources :boats do
-      resources :crew_members
-    end
-    resources :bookings
-    resources :reviews, only: [:index, :show]
-  end
+  get "/dashboard", to: "pages#dashboard", as: :dashboard
+  get "/wishlist", to: "pages#wishlist", as: :wishlist
+  get "/messages", to: "pages#messages", as: :messages
+  get "/payment-methods", to: "pages#payment_methods", as: :payment_methods
 
-  resources :boats do
+  resources :boats, only: %i[index show] do
     resources :crew_members
   end
 
-  resources :bookings, only: [:index, :show, :new, :create, :destroy] do
-    resources :reviews, only: [:new, :create]
+  resources :listings
+
+  resources :bookings, only: %i[index show new create destroy] do
+    resources :reviews, only: %i[new create]
   end
 
-  resources :reviews, only: [:destroy]
-  resources :users, only: [:show, :edit, :update, :destroy]
+  resources :users, only: %i[show edit update destroy]
 end
