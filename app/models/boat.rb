0ac1db0401242b0f4ff6aa7309eb2a9fad3f9488
Patricia Bertran <pbertranc@gmail.com>
2023-08-23 23:27:13 +0200

    adding map

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,4 +4,7 @@ class Boat < ApplicationRecord
   has_many :crew_members, dependent: :destroy
 
   CATEGORIES = ["Sailboat", "Motorboat", "Catamaran", "Yacht", "Pirate Ship", "Cargo Ship", "Inflatable Unicorn"]
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
